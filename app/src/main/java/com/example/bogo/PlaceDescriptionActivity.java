94a82a116c75d81f20b7ee67cee2f8fdb3d42a07
Sergio A. Mejía <sergio.mejia@javeriana.edu.co>
2020-09-05 18:19:44 -0500

    Added PlaceDescription layout - no reviews

---
+++ app/src/main/java/com/example/bogo/PlaceDescriptionActivity.java
@@ -0,0 +1,14 @@
+package com.example.bogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlaceDescriptionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_description);
+    }
+}
