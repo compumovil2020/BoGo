1973672b9f21814f6d8e47a67b8f4008c915e3b4
Julian David Parada Galvis <julian_parada@javeriana.edu.co>
2020-09-07 16:50:45 -0500

    Added intent from AddReview

--- app/src/main/java/com/example/bogo/AddReviewActivity.java
+++ app/src/main/java/com/example/bogo/AddReviewActivity.java
@@ -2,13 +2,28 @@ package com.example.bogo;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AddReviewActivity extends AppCompatActivity {
+
+    Button buttonEnviar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_review);
+
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+
+        buttonEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getBaseContext(), PlaceDescriptionActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
