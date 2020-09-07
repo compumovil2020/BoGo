635c069d1e365ad243216e6ab809b49731bb713f
Julian David Parada Galvis <julian_parada@javeriana.edu.co>
2020-09-06 23:20:59 -0500

    Full Interfaces fuck (funciona porfi)

---
+++ app/src/main/java/com/example/bogo/AddReviewActivity.java
@@ -0,0 +1,14 @@
+package com.example.bogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddReviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
+    }
+}
