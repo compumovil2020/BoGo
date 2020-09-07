d5ea926bbfb2d1e2331a5a0132cfbfdbe34125da
Sergio A. Mejía <sergio.mejia@javeriana.edu.co>
2020-09-06 23:37:17 -0500

    Finished NavigationDrawer with connections

--- app/src/main/java/com/example/bogo/AddPlaceActivity.java
+++ app/src/main/java/com/example/bogo/AddPlaceActivity.java
@@ -1,14 +1,20 @@
 package com.example.bogo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class AddPlaceActivity extends AppCompatActivity {
+public class AddPlaceActivity extends Fragment {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_add_place);
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        final View view = inflater.inflate(R.layout.activity_add_place, container, false);
+
+        return view;
     }
 }
