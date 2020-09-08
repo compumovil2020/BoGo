346f8410527cf0d0cf352627ff6709c60b970e64
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-09-07 20:47:53 -0500

    Created Redeem Points Activity

---
+++ app/src/main/java/com/example/bogo/RedeemPointsActivity.java
@@ -0,0 +1,14 @@
+package com.example.bogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RedeemPointsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redeem_points);
+    }
+}
