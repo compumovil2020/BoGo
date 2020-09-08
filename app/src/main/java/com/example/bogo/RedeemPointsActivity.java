492d6b62c1f440a1c789b7fd2808bb9a9c010bab
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-09-07 22:14:16 -0500

    Added Redeem Points Activity

--- app/src/main/java/com/example/bogo/RedeemPointsActivity.java
+++ app/src/main/java/com/example/bogo/RedeemPointsActivity.java
@@ -3,6 +3,8 @@ package com.example.bogo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class RedeemPointsActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class RedeemPointsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_redeem_points);
+
+
+        LinearLayout llRedeemPoints = findViewById(R.id.llLayoutRedeemPoints);
+
+        for(int i = 0; i < 5; i++)
+        {
+            View child = getLayoutInflater().inflate(R.layout.layout_redeem_points, null);
+            llRedeemPoints.addView(child);
+        }
     }
+
+
 }
