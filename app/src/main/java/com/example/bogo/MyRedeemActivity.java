dc3d977990811c3b595da95e755be087b1a10651
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-09-07 22:40:13 -0500

    Created My Redeem Activity

---
+++ app/src/main/java/com/example/bogo/MyRedeemActivity.java
@@ -0,0 +1,14 @@
+package com.example.bogo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MyRedeemActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_redeem);
+    }
+}
