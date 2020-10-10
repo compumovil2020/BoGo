1ee2ad650681a877d53f69bd1938a6d767e136bb
Sebastian <sebasgut.5@gmail.com>
2020-10-10 16:02:42 -0500

    PlaceMap Without routes

--- app/src/main/java/com/example/bogo/PlaceMapActivity.java
+++ app/src/main/java/com/example/bogo/PlaceMapActivity.java
@@ -1,12 +1,65 @@
 package com.example.bogo;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.graphics.Point;
+import android.graphics.drawable.Drawable;
+import android.hardware.SensorManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.bogo.Utils.PermissionsManager;
+import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import org.osmdroid.api.IMapController;
+import org.osmdroid.api.IMapView;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapController;
+import org.osmdroid.views.MapView;
+import org.osmdroid.views.overlay.ItemizedIconOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlay;
+import org.osmdroid.views.overlay.ItemizedOverlayWithFocus;
+import org.osmdroid.views.overlay.Marker;
+import org.osmdroid.views.overlay.OverlayItem;
 
 public class PlaceMapActivity extends AppCompatActivity {
     Button btnCarOption, btnBusOption, btnWalkOption;
+    MapView mMap;
+    IMapController mapController;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    GeoPoint actual;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -15,5 +68,135 @@ public class PlaceMapActivity extends AppCompatActivity {
         btnCarOption = findViewById(R.id.btnCarOption);
         btnBusOption = findViewById(R.id.btnBusOption);
         btnWalkOption = findViewById(R.id.btnWalOption);
+        GeoPoint buffalo = new GeoPoint(4.618534,-74.068002);
+        final Context ctx = getApplicationContext();
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        PermissionsManager.requestPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE,
+                "Para poder cargar el mapa", PermissionsManager.READ_STORAGE_PERMISSION);
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+            mMap =  findViewById(R.id.openmapview);
+            mMap.setTileSource(TileSourceFactory.MAPNIK);
+            mapController = mMap.getController();
+            mMap.setMultiTouchControls(true);
+            Marker placeMarker = new Marker(mMap);
+            placeMarker.setIcon(getDrawable(R.drawable.btnpincho));
+            placeMarker.setPosition(buffalo);
+            placeMarker.setTitle("Lugar seleccionado");
+            placeMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+            mMap.getOverlays().add(placeMarker);
+            mapController.setZoom(20.0);
+            mapController.setCenter(buffalo);
+
+            mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            mLocationRequest = createLocationRequest();
+            mLocationCallback = new LocationCallback(){
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    super.onLocationResult(locationResult);
+                    Location location = locationResult.getLastLocation();
+                    if(location!=null)
+                    {
+                        actual = new GeoPoint(location);
+                        Marker locationMarker = new Marker(mMap);
+                        locationMarker.setIcon(getDrawable(R.drawable.btnpincho));
+                        locationMarker.setPosition(actual);
+                        locationMarker.setTitle("Ubicación actual");
+                        locationMarker.setAnchor(Marker.ANCHOR_CENTER, Marker.ANCHOR_BOTTOM);
+                        mMap.getOverlays().add(locationMarker);
+                    }
+                }
+            };
+            PermissionsManager.requestPermission(this,Manifest.permission.ACCESS_FINE_LOCATION,"Es necesario para tener la ubicación del usuario",PermissionsManager.LOCATION_PERMISSION);
+            usePermition();
+            if(actual!=null)
+            {
+               // RoadManager roadManager = new OSRMRoadManager(this);
+            }
+        }
+
+    }
+    private void usePermition() {
+        if(ContextCompat.checkSelfPermission(getBaseContext(),Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED)
+        {
+            checkSettings();
+        }
+    }
+
+    private void checkSettings()
+    {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        SettingsClient client =  LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                startLocationUpdates();
+            }
+        });
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode){
+                    case CommonStatusCodes.RESOLUTION_REQUIRED: {
+                        try {
+                            ResolvableApiException resolvable = (ResolvableApiException) e;
+                            resolvable.startResolutionForResult(PlaceMapActivity.this ,PermissionsManager.REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException sendEx) {
+                        }
+                        break;
+                    }
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:{
+                        Toast.makeText(getBaseContext(),"No se pudo activar la localización",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==PermissionsManager.REQUEST_CHECK_SETTINGS && resultCode==RESULT_OK){
+            startLocationUpdates();
+        }else
+        {
+            Toast.makeText(this, "Sin acceso a localización, hardware deshabilitado!", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void startLocationUpdates()
+    {
+        if(ContextCompat.checkSelfPermission(getBaseContext(),Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED)
+        {
+            mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest,mLocationCallback,null);
+        }
+    }
+
+    protected LocationRequest createLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        //locationRequest.setInterval(10000);
+        //locationRequest.setFastestInterval(5000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    private void stopLocationUpdates()
+    {
+        mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startLocationUpdates();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        stopLocationUpdates();
     }
 }
---
+++ app/src/main/java/com/example/bogo/Utils/PermissionsManager.java
@@ -0,0 +1,36 @@
+package com.example.bogo.Utils;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+public class PermissionsManager
+{
+    public static final int READ_CONTACTS_PERMISSION = 1;
+    public static final int CAMERA_PERMISSION = 2;
+    public static final int READ_STORAGE_PERMISSION = 3;
+    public static final int LOCATION_PERMISSION = 11;
+    public static final int REQUEST_CHECK_SETTINGS = 22;
+
+    public static void requestPermission(Activity context, String permiso, String justificacion, int idCode)
+    {
+        // Here, thisActivity is the current activity
+        if (ContextCompat.checkSelfPermission(context, permiso) != PackageManager.PERMISSION_GRANTED)
+        {
+            // Should we show an explanation?
+            if( ActivityCompat.shouldShowRequestPermissionRationale(context, permiso) )
+            {
+                Toast.makeText(context, justificacion, Toast.LENGTH_LONG).show();
+            }
+            // request the permission.
+            ActivityCompat.requestPermissions(context, new String[]{permiso}, idCode);
+            // MY_PERMISSIONS_REQUEST_READ_CONTACTS es una
+            // constante definida en la aplicación, se debe usar
+            // en el callback para identificar el permiso
+        }
+    }
+}
