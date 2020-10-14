80245da623e68066b380111611760e34ed1f4915
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-10-13 21:46:31 -0500

    Added photos add review activity

--- app/src/main/java/com/example/bogo/Activities/AddReviewActivity.java
+++ app/src/main/java/com/example/bogo/Activities/AddReviewActivity.java
@@ -1,31 +1,24 @@
 package com.example.bogo.Activities;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.bogo.Utils.PermissionsManager;
-
-import org.osmdroid.config.Configuration;
 
 import java.io.FileNotFoundException;
 import java.io.InputStream;
@@ -37,21 +30,17 @@ public class AddReviewActivity extends AppCompatActivity {
     Button buttonEnviar, buttonAddPhoto, buttonAddCamera;
     TextView txtFotosAgregadas;
     ImageView imgAddPlace;
-    View view = null;
 
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_review);
 
-        view = inflater.inflate(R.layout.activity_add_review, container, false);
-        buttonEnviar = view.findViewById(R.id.buttonEnviar);
-        buttonAddPhoto = view.findViewById(R.id.btnAddFoto);
-        buttonAddCamera = view.findViewById(R.id.btnAddCamera);
-        txtFotosAgregadas = view.findViewById(R.id.textFotosAgregadas);
-        imgAddPlace = view.findViewById(R.id.imgAddPlace);
-
-        final Context ctx = view.getContext();
-        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
-        Configuration.getInstance().setUserAgentValue(BuildConfig.APPLICATION_ID);
+        buttonEnviar = findViewById(R.id.buttonEnviar);
+        buttonAddPhoto = findViewById(R.id.btnAddFoto);
+        buttonAddCamera = findViewById(R.id.btnAddCamera);
+        txtFotosAgregadas = findViewById(R.id.textFotosAgregadas);
+        imgAddPlace = findViewById(R.id.imgAddPlace);
 
         buttonEnviar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -74,28 +63,28 @@ public class AddReviewActivity extends AppCompatActivity {
                 addPhotoFromCamera();
             }
         });
-
-        return view;
     }
 
     private void addPhotoFromCamera()
     {
-        PermissionsManager.requestPermission(this, Manifest.permission.CAMERA,
+        PermissionsManager.requestPermission((Activity) this, Manifest.permission.CAMERA,
                 "Para poder mostrar fotos tomadas desde su cámara", PermissionsManager.CAMERA_PERMISSION );
-        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED)
+        if (ContextCompat.checkSelfPermission(getBaseContext() , Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED)
         {
             Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
             if (takePictureIntent.resolveActivity(this.getPackageManager()) != null) {
                 startActivityForResult(takePictureIntent, PermissionsManager.REQUEST_IMAGE_CAPTURE);
             }
         }
+
+
     }
 
     private void addPhotoFromGallery()
     {
-        PermissionsManager.requestPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE,
+        PermissionsManager.requestPermission((Activity)this, Manifest.permission.READ_EXTERNAL_STORAGE,
                 "Para poder mostrar fotos que ya tenga guardadas", PermissionsManager.READ_STORAGE_PERMISSION);
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+        if(ContextCompat.checkSelfPermission((Activity)this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
         {
             Intent pickImage = new Intent(Intent.ACTION_PICK);
             pickImage.setType("image/*");
@@ -141,7 +130,6 @@ public class AddReviewActivity extends AppCompatActivity {
                     Bitmap imageBitmap = (Bitmap) extras.get("data");
                     txtFotosAgregadas.setText("Imagen agregada!");
                     imgAddPlace.setImageBitmap(imageBitmap);
-                    imgAddPlace.getLayoutParams().height = (int) (100.0 * view.getContext().getResources().getDisplayMetrics().density);
                     imgAddPlace.requestLayout();
                     break;
                 case PermissionsManager.IMAGE_PICKER_REQUEST:
@@ -150,7 +138,7 @@ public class AddReviewActivity extends AppCompatActivity {
                         final InputStream imageStream = this.getContentResolver().openInputStream(imageUri);
                         final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
                         imgAddPlace.setImageBitmap(selectedImage);
-                        imgAddPlace.getLayoutParams().height = (int) (100.0 * view.getContext().getResources().getDisplayMetrics().density);
+                        imgAddPlace.setScaleType(ImageView.ScaleType.FIT_XY);
                         imgAddPlace.requestLayout();
                         txtFotosAgregadas.setText("Imagen agregada!");
                     } catch (FileNotFoundException e) {
