40f1f2a22759058c4d42047f897ae245cd141a3a
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-10-17 12:34:29 -0500

    Created Activity Add Friends

---
+++ app/src/main/java/com/example/bogo/Activities/AddFriendActivity.java
@@ -0,0 +1,16 @@
+package com.example.bogo.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bogo.R;
+
+public class AddFriendActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_friend);
+    }
+}
