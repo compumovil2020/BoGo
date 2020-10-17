69f6c44fbcc621afc72e37aaf957e869e90ecdc7
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-10-17 13:46:50 -0500

    Added Adapter New Friends

--- app/src/main/java/com/example/bogo/Activities/AddFriendActivity.java
+++ app/src/main/java/com/example/bogo/Activities/AddFriendActivity.java
@@ -3,14 +3,56 @@ package com.example.bogo.Activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ListView;
 
+import com.example.bogo.Adapters.AddFriendAdapter;
+import com.example.bogo.Adapters.FriendAdapter;
 import com.example.bogo.R;
 
+import java.util.ArrayList;
+
 public class AddFriendActivity extends AppCompatActivity {
+
+    ListView lstNewFriends;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+
+        lstNewFriends = findViewById(R.id.lstFindFriends);
+
+        ArrayList<AddFriendActivity.NewFriend> contenido = new ArrayList<>();
+        AddFriendActivity.NewFriend f1 = new AddFriendActivity.NewFriend("Pepe Pérez", "ppperez12");
+        AddFriendActivity.NewFriend f2 = new AddFriendActivity.NewFriend("Julian Parada", "JulianParada");
+        AddFriendActivity.NewFriend f3 = new AddFriendActivity.NewFriend("Sebastián Gutiérrez", "sebasgut1");
+        AddFriendActivity.NewFriend f4 = new AddFriendActivity.NewFriend("Mónica Álvarez", "monicaleja99");
+        AddFriendActivity.NewFriend f5 = new AddFriendActivity.NewFriend("Laura Jiménez", "LauraMJimenezx2");
+        AddFriendActivity.NewFriend f6 = new AddFriendActivity.NewFriend("Sergio Mejia", "iSergioMejia");
+        contenido.add(f1); contenido.add(f2); contenido.add(f3);
+        contenido.add(f4); contenido.add(f5); contenido.add(f6);
+
+        AddFriendAdapter adapter = new AddFriendAdapter(getBaseContext(), contenido);
+        lstNewFriends.setAdapter(adapter);
+
+
+    }
+
+    public class NewFriend{ //ESTO DEBE REEMPLAZARSE POR UNA CLASE DEL MODELO
+        String nombre;
+        String user;
+
+        public NewFriend(String nombre, String user) {
+            this.nombre = nombre;
+            this.user = user;
+        }
+
+        public String getNombre() {
+            return nombre;
+        }
+
+        public String getUser() {
+            return user;
+        }
     }
 }
---
+++ app/src/main/java/com/example/bogo/Adapters/AddFriendAdapter.java
@@ -0,0 +1,44 @@
+package com.example.bogo.Adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.bogo.Activities.AddFriendActivity;
+import com.example.bogo.R;
+import java.util.ArrayList;
+
+public class AddFriendAdapter extends ArrayAdapter<AddFriendActivity.NewFriend> {
+    private final Context context;
+    private final ArrayList<AddFriendActivity.NewFriend> values;
+
+    public AddFriendAdapter(Context context, ArrayList<AddFriendActivity.NewFriend> values) {
+        super(context, R.layout.adapter_add_friend,values);
+        this.context = context;
+        this.values = values;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View rowView = inflater.inflate(R.layout.adapter_add_friend, parent, false);
+        TextView txtFriendName = rowView.findViewById(R.id.txtFriendName);
+        TextView txtFriendUser =  rowView.findViewById(R.id.txtFriendUser);
+        ImageView imgFriend = rowView.findViewById(R.id.imgFotoAmigo);
+        ImageButton imgAdd = rowView.findViewById(R.id.imgAdd);
+
+        txtFriendName.setText(this.values.get(position).getNombre());
+        txtFriendUser.setText(this.values.get(position).getUser());
+        imgFriend.setImageResource(R.drawable.ic_profilepic);
+        imgAdd.setImageResource(R.drawable.ic_plus);
+        return rowView;
+
+    }
+
+
+}
