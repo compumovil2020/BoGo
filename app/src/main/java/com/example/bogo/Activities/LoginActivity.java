b12e96e59463854ba9e9cf2b211af6e4d2c5be21
M <monik9912@hotmail.com>
2020-10-16 20:24:41 -0500

    First configuration to login with FB

--- app/src/main/java/com/example/bogo/Activities/LoginActivity.java
+++ app/src/main/java/com/example/bogo/Activities/LoginActivity.java
@@ -26,7 +26,6 @@ public class LoginActivity extends AppCompatActivity {
     Button btnLogin, btnFBLogin, btnGoogleLogin, btnRegisterLogin;
     private FirebaseAuth mAuth;
 
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
