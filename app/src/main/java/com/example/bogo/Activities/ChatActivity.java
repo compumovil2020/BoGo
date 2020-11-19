de7ed2f0bd6eae659abde6b99c97556d2c49331b
Sebastian <sebasgut.5@gmail.com>
2020-11-18 22:28:04 -0500

    Chat almost finished

--- app/src/main/java/com/example/bogo/Activities/ChatActivity.java
+++ app/src/main/java/com/example/bogo/Activities/ChatActivity.java
@@ -1,9 +1,11 @@
 package com.example.bogo.Activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Adapter;
 import android.widget.Button;
@@ -11,42 +13,70 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.Toolbar;
 
 import com.example.bogo.Adapters.MensajeAdapter;
+import com.example.bogo.Entidades.Mensaje;
 import com.example.bogo.R;
+import com.example.bogo.Utils.Utils;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.HashMap;
 
 public class ChatActivity extends AppCompatActivity {
+
+    FirebaseAuth mAuth;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+
+    String chatId;
+    String amigoId;
+    ArrayList<Mensaje> contenido;
 
     ListView lstMensajes;
     ImageButton btnSend;
     ImageView seguimiento;
     EditText edtSend;
+    TextView nombre;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
 
+        mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
+
         seguimiento = findViewById(R.id.btnSeguimiento);
 
         lstMensajes = findViewById(R.id.lstMensajes);
         btnSend = findViewById(R.id.btnSendMessage);
         edtSend = findViewById(R.id.edtMessageInput);
+        nombre = findViewById(R.id.txtNombreChat);
 
-        final ArrayList<Mensaje> contenido = new ArrayList<>();
-        String mensaje1 = "Hola, ¿Cómo vas?";
+        Intent intent = getIntent();
+        amigoId = intent.getStringExtra("idAmigo");
+        nombre.setText(intent.getStringExtra("nombreAmigo"));
+
+        contenido = new ArrayList<>();
+       /* String mensaje1 = "Hola, ¿Cómo vas?";
         String mensaje2 = "Bien ¿Y tú qué tal?";
         Mensaje nuevo1 = new Mensaje(mensaje1,true);
         Mensaje nuevo2 = new Mensaje(mensaje2,false);
         contenido.add(nuevo1);
-        contenido.add(nuevo2);
+        contenido.add(nuevo2);*/
 
-        MensajeAdapter adapter = new MensajeAdapter(getBaseContext(),contenido);
-        lstMensajes.setAdapter(adapter);
+        obtenerChat(mAuth.getUid());
+
 
         btnSend.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,11 +84,13 @@ public class ChatActivity extends AppCompatActivity {
                 String mensaje = edtSend.getText().toString();
                 if(!mensaje.isEmpty())
                 {
-                    Mensaje enviar =  new Mensaje(mensaje,true);
+                    Long hora = Calendar.getInstance().getTimeInMillis();
+                    Mensaje enviar =  new Mensaje(mensaje,mAuth.getUid(),hora );
                     contenido.add(enviar);
                     MensajeAdapter adapNuevo = new MensajeAdapter(getBaseContext(),contenido);
                     lstMensajes.setAdapter(adapNuevo);
                     edtSend.setText("");
+                    enviarMensaje(enviar);
                 }
             }
         });
@@ -73,22 +105,60 @@ public class ChatActivity extends AppCompatActivity {
         });
     }
 
-    public class Mensaje{ //ESTO DEBE REEMPLAZARSE POR UNA CLASE DEL MODELO
-        String mensaje;
-        Boolean tipo; //False Recibido - True Enviado
+    void obtenerChat(final String uid)
+    {
+        myRef = database.getReference(Utils.PATH_CHATS);
+        myRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                ArrayList<String> ids = new ArrayList<>();
+                for (DataSnapshot dataSnap : dataSnapshot.getChildren()) {
+                    for(DataSnapshot users : dataSnap.getChildren())
+                    {
+                        ids.add(users.getKey());
+                    }
+                    if(ids.contains(uid) && ids.contains(amigoId))
+                    {
+                        chatId = dataSnap.getKey();
+                    }
+                    ids.clear();
+                }
+                verMensajes();
+            }
 
-        public Mensaje(String mensaje, Boolean tipo) {
-            this.mensaje = mensaje;
-            this.tipo = tipo;
-        }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
 
-        public String getMensaje() {
-            return mensaje;
-        }
+            }
+        });
+    }
 
-        public Boolean getTipo() {
-            return tipo;
-        }
+    void verMensajes()
+    {
+        myRef = database.getReference(Utils.PATH_MENSAJES + chatId+"/");
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                for (DataSnapshot dataSnap : dataSnapshot.getChildren()) {
+                   Mensaje mensaje = dataSnap.getValue(Mensaje.class);
+                   contenido.add(mensaje);
+                }
+                MensajeAdapter adapter = new MensajeAdapter(getBaseContext(),contenido);
+                lstMensajes.setAdapter(adapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+   void enviarMensaje(Mensaje nuevo){
+        myRef = database.getReference(Utils.PATH_MENSAJES + chatId+"/");
+        String key = myRef.push().getKey();
+        myRef = database.getReference(Utils.PATH_MENSAJES + chatId +"/"+key);
+        myRef.setValue(nuevo);
     }
 
 }
