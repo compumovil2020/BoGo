f7044d9928079012824dce03a055f945a3717cf3
Laura Mariana Jimenez <lauramarianajimenez@gmail.com>
2020-11-28 20:25:48 -0500

    Created Adapter Place

--- app/src/main/java/com/example/bogo/Activities/PlaceList.java
+++ app/src/main/java/com/example/bogo/Activities/PlaceList.java
@@ -19,38 +19,6 @@ public class PlaceList extends AppCompatActivity {
 
         LinearLayout llPlace1 = findViewById(R.id.llPlace1);
 
-        for(int i = 0; i < 3; i++)
-        {
-            final View child = getLayoutInflater().inflate(R.layout.layout_place, null);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(
-                    LinearLayout.LayoutParams.MATCH_PARENT,
-                    LinearLayout.LayoutParams.WRAP_CONTENT
-            );
-            params.setMargins(0, 0, 0, 10);
-            child.setLayoutParams(params);
 
-            ConstraintLayout Clplace1 = child.findViewById(R.id.constraintLayout3);
-            Clplace1.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(getBaseContext(),PlaceDescriptionActivity.class);
-                    startActivity(intent);
-                }
-
-            });
-
-            ConstraintLayout Clplace2 = child.findViewById(R.id.constraintLayout4);
-            Clplace2.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(getBaseContext(), PlaceDescriptionActivity.class);
-                    startActivity(intent);
-                }
-            });
-
-            llPlace1.addView(child);
-
-
-        }
     }
 }
