46662b54e20d01d055086fff4c950c4e6f035fa3
M <monik9912@hotmail.com>
2020-12-01 19:37:00 -0500

    Register done with add profile photo

--- app/src/main/java/com/example/bogo/Activities/RegisterActivity.java
+++ app/src/main/java/com/example/bogo/Activities/RegisterActivity.java
@@ -2,16 +2,29 @@ package com.example.bogo.Activities;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
+import android.content.IntentSender;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.location.Location;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.bogo.Entidades.Usuario;
 import com.example.bogo.R;
+import com.example.bogo.Utils.PermissionsManager;
 import com.example.bogo.Utils.Utils;
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
@@ -25,7 +38,20 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
 import com.google.android.gms.common.SignInButton;
 import com.google.android.gms.common.api.ApiException;
+import com.google.android.gms.common.api.CommonStatusCodes;
+import com.google.android.gms.common.api.ResolvableApiException;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.AuthCredential;
@@ -34,18 +60,37 @@ import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
 
 public class RegisterActivity extends AppCompatActivity {
     private static final String TAG = "REGISTER";
     private static final int RC_SIGN_IN = 2;
     EditText edtNombreRegister, edtUserRegister, edtEmailRegister, edtPasswordRegister, edtConfirmPassword;
-    Button btnRegister;
+    Button btnRegister, btnCamera, btnGallery;
     LoginButton btnFBRegister;
     SignInButton btnGoogleRegister;
     private FirebaseAuth mAuth;
     private CallbackManager mCallbackManager;
     GoogleSignInClient mGoogleSignInClient;
-
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private LocationRequest mLocationRequest;
+    private LocationCallback mLocationCallback;
+    private StorageReference mStorageRef;
+    FirebaseDatabase database;
+    DatabaseReference myRef;
+    FirebaseUser mUser;
+    double lat, lon;
+    boolean intentRegister = false;
+    Uri miImagenUri;
+    byte[] miImagenBit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -56,6 +101,8 @@ public class RegisterActivity extends AppCompatActivity {
         edtEmailRegister = findViewById(R.id.edtEmailRegister);
         edtPasswordRegister = findViewById(R.id.edtPasswordRegister);
         edtConfirmPassword = findViewById(R.id.edtConfirmPassword);
+        btnCamera = findViewById(R.id.btnCamProfile);
+        btnGallery = findViewById(R.id.btnGalProfile);
         //Botones
         btnRegister = findViewById(R.id.btnRegister);
         btnFBRegister = findViewById(R.id.btnFBReg);
@@ -63,7 +110,39 @@ public class RegisterActivity extends AppCompatActivity {
         btnGoogleRegister.setSize(SignInButton.SIZE_STANDARD);
 
         mAuth = FirebaseAuth.getInstance();
+        database = FirebaseDatabase.getInstance();
         mCallbackManager = CallbackManager.Factory.create();
+        mStorageRef = FirebaseStorage.getInstance().getReference();
+
+        btnGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addPhotoFromGallery();
+            }
+        });
+
+        btnCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                addPhotoFromCamera();
+            }
+        });
+
+        //Obtener localizacion
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+        mLocationRequest = createLocationRequest();
+        mLocationCallback = new LocationCallback(){
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                super.onLocationResult(locationResult);
+                Location my_location = locationResult.getLastLocation();
+                if(my_location!=null) {
+                    lat = my_location.getLatitude();
+                    lon = my_location.getLongitude();
+                    Log.i("UBICACION", "Mi latitud es: " + lat + "- Mi longitud es: " + lon);
+                }
+            }
+        };
 
         //Email&Password
         btnRegister.setOnClickListener(new View.OnClickListener() {
@@ -120,10 +199,13 @@ public class RegisterActivity extends AppCompatActivity {
                 signUp();
             }
         });
+        requestPermission(this, Manifest.permission.ACCESS_FINE_LOCATION,"Es necesario para el funcionamiento correcto de la APP.", PermissionsManager.LOCATION_PERMISSION);
+        usePermition();
     }
 
     //Email&Password
     private void signUp() {
+        intentRegister = true;
         String email = this.edtEmailRegister.getText().toString();
         String password = this.edtPasswordRegister.getText().toString();
 
@@ -172,8 +254,19 @@ public class RegisterActivity extends AppCompatActivity {
                             if (task.isSuccessful()) {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d(TAG, "createUserWithEmail:success");
-                                FirebaseUser user = mAuth.getCurrentUser();
-                                updateUI(user);
+                                mUser = mAuth.getCurrentUser();
+                                Usuario user = new Usuario();
+                                user.setNombre(edtNombreRegister.getText().toString());
+                                user.setNombreUsuario(edtUserRegister.getText().toString());
+                                user.setLatitud(lat);
+                                user.setLongitud(lon);
+                                Log.i("REGISTRO", "tengo mUser con " + mUser);
+                                Log.i("REGISTRO", "tengo mUser con " + mAuth.getCurrentUser());
+                                myRef = database.getReference(Utils.PATH_USUARIOS + mUser.getUid());
+                                myRef.setValue(user);
+
+                                uploadFile(mUser.getUid());
+                                updateUI(mUser);
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Log.w(TAG, "createUserWithEmail:failure", task.getException());
@@ -183,12 +276,14 @@ public class RegisterActivity extends AppCompatActivity {
                             // ...
                         }
                     });
-        }
-    }
 
+        }
+
+    }
 
     //Facebook
     private void handleFacebookAccessToken(AccessToken token) {
+        intentRegister = true;
         Log.d(TAG, "handleFacebookAccessToken:" + token);
 
         AuthCredential credential = FacebookAuthProvider.getCredential(token.getToken());
@@ -215,6 +310,7 @@ public class RegisterActivity extends AppCompatActivity {
 
     //Google
     private void signIn() {
+        intentRegister = true;
         Intent signInIntent = mGoogleSignInClient.getSignInIntent();
         startActivityForResult(signInIntent, RC_SIGN_IN);
     }
@@ -250,9 +346,11 @@ public class RegisterActivity extends AppCompatActivity {
             finish();
         } else {
             View parentLayout = findViewById(android.R.id.content);
-            Snackbar.make(parentLayout,
-                    "No se pudo registrar el usuario nuevo",
-                    Snackbar.LENGTH_LONG).show();
+            if(intentRegister==true){
+                Snackbar.make(parentLayout,
+                        "No se pudo registrar el usuario nuevo",
+                        Snackbar.LENGTH_LONG).show();
+            }
         }
     }
 
@@ -261,6 +359,30 @@ public class RegisterActivity extends AppCompatActivity {
         super.onStart();
         FirebaseUser currentUser = mAuth.getCurrentUser();
         updateUI(currentUser);
+    }
+
+    private void addPhotoFromCamera()
+    {
+        PermissionsManager.requestPermission((Activity) this, Manifest.permission.CAMERA,
+                "Para poder mostrar fotos tomadas desde su cámara", PermissionsManager.CAMERA_PERMISSION );
+        if (ContextCompat.checkSelfPermission(getBaseContext() , Manifest.permission.CAMERA)== PackageManager.PERMISSION_GRANTED)
+        {
+            Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            if (takePictureIntent.resolveActivity(this.getPackageManager()) != null) {
+                startActivityForResult(takePictureIntent, PermissionsManager.REQUEST_IMAGE_CAPTURE);
+            }
+        }
+    }
+
+    private void addPhotoFromGallery()
+    { PermissionsManager.requestPermission((Activity)this, Manifest.permission.READ_EXTERNAL_STORAGE,
+            "Para poder mostrar fotos que ya tenga guardadas", PermissionsManager.READ_STORAGE_PERMISSION);
+        if(ContextCompat.checkSelfPermission((Activity)this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+        {
+            Intent pickImage = new Intent(Intent.ACTION_PICK);
+            pickImage.setType("image/*");
+            startActivityForResult(pickImage, PermissionsManager.IMAGE_PICKER_REQUEST);
+        }
     }
 
     @Override
@@ -284,5 +406,144 @@ public class RegisterActivity extends AppCompatActivity {
                 // ...
             }
         }
+        if(resultCode == RESULT_OK) {
+            switch (requestCode) {
+                case PermissionsManager.REQUEST_IMAGE_CAPTURE:
+                    Bundle extras = data.getExtras();
+                    Bitmap imageBitmap = (Bitmap) extras.get("data");
+
+                    ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                    imageBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos);
+                    miImagenBit = baos.toByteArray();
+
+                    break;
+                case PermissionsManager.IMAGE_PICKER_REQUEST:
+                    try {
+                        miImagenUri = data.getData();
+                        final InputStream imageStream = getContentResolver().openInputStream(miImagenUri);
+                        final Bitmap selectedImage = BitmapFactory.decodeStream(imageStream);
+
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    }
+                    break;
+
+            }
+        }
     }
+
+    private void uploadFile(String key) {
+        StorageReference userRef = mStorageRef.child(Utils.PATH_USUARIOS + key +"/profile.jpg");
+        if(miImagenUri != null)
+        {
+            userRef.putFile(miImagenUri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Get a URL to the uploaded content
+                            Intent intent = new Intent(getBaseContext(), DropMenuActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            finish();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                            // ...
+                        }
+                    });
+        }else
+        {
+            userRef.putBytes(miImagenBit)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            // Get a URL to the uploaded content
+                            Intent intent = new Intent(getBaseContext(), DropMenuActivity.class);
+                            intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                            startActivity(intent);
+                            finish();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+                            // Handle unsuccessful uploads
+                            // ...
+                        }
+                    });
+        }
+
+    }
+
+    //Permisos
+    private void usePermition() {
+        if(ContextCompat.checkSelfPermission(getBaseContext(), Manifest.permission.ACCESS_FINE_LOCATION)== PackageManager.PERMISSION_GRANTED) {
+            checkSettings();
+        }
+    }
+
+    private void checkSettings() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder().addLocationRequest(mLocationRequest);
+        SettingsClient client =  LocationServices.getSettingsClient(this);
+        Task<LocationSettingsResponse> task = client.checkLocationSettings(builder.build());
+
+        task.addOnSuccessListener(this, new OnSuccessListener<LocationSettingsResponse>() {
+            @Override
+            public void onSuccess(LocationSettingsResponse locationSettingsResponse) {
+                startLocationUpdates();
+            }
+        });
+        task.addOnFailureListener(this, new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                int statusCode = ((ApiException) e).getStatusCode();
+                switch (statusCode){
+                    case CommonStatusCodes.RESOLUTION_REQUIRED: {
+                        try {
+                            ResolvableApiException resolvable = (ResolvableApiException) e;
+                            resolvable.startResolutionForResult(RegisterActivity.this,PermissionsManager.REQUEST_CHECK_SETTINGS);
+                        } catch (IntentSender.SendIntentException sendEx) {
+                        }
+                        break;
+                    }
+                    case LocationSettingsStatusCodes.SETTINGS_CHANGE_UNAVAILABLE:{
+                        Toast.makeText(getBaseContext(),"No se pudo completar la operación.",Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+    }
+
+    private void startLocationUpdates() {
+        if(ContextCompat.checkSelfPermission(getBaseContext(),Manifest.permission.ACCESS_FINE_LOCATION)==PackageManager.PERMISSION_GRANTED) {
+            mFusedLocationProviderClient.requestLocationUpdates(mLocationRequest,mLocationCallback,null);
+        }
+    }
+
+    protected LocationRequest createLocationRequest() {
+        LocationRequest locationRequest = new LocationRequest();
+        locationRequest.setInterval(70000);
+        locationRequest.setFastestInterval(70000);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        return locationRequest;
+    }
+
+    private void requestPermission(Activity context, String permiso, String justificacion, int idCode) {
+        if(ContextCompat.checkSelfPermission(context,permiso) != PackageManager.PERMISSION_GRANTED)
+        {
+            if(ActivityCompat.shouldShowRequestPermissionRationale(context, permiso)) {
+                //Show an explanation to user asynchronously
+            }
+            //request permission
+            ActivityCompat.requestPermissions(context,new String[]{permiso},idCode);
+        }
+    }
+
+    private void stopLocationUpdates() {
+        mFusedLocationProviderClient.removeLocationUpdates(mLocationCallback);
+    }
+
 }
